6ce95176213640ac2016c4a27f41cef530552d3a
priyankgit11 <140704202+priyankgit11@users.noreply.github.com>
2023-09-27 18:39:46 +0530

    Added SQL FUNCTION sql

---
+++ window_functions.sql
@@ -0,0 +1,37 @@
+CREATE TABLE employees (
+    emp_no INT PRIMARY KEY IDENTITY(1,1),
+    department VARCHAR(20),
+    salary INT
+);
+
+INSERT INTO employees (department, salary) VALUES
+('engineering', 80000),
+('engineering', 69000),
+('engineering', 70000),
+('engineering', 103000),
+('engineering', 67000),
+('engineering', 89000),
+('engineering', 91000),
+('sales', 59000),
+('sales', 70000),
+('sales', 159000),
+('sales', 72000),
+('sales', 60000),
+('sales', 61000),
+('sales', 61000),
+('customer service', 38000),
+('customer service', 45000),
+('customer service', 61000),
+('customer service', 40000),
+('customer service', 31000),
+('customer service', 56000),
+('customer service', 55000);
+
+SELECT * FROM employees;
+SELECT emp_no,department,salary,MIN(salary) OVER(),MAX(salary) OVER() FROM employees;
+SELECT  MAX(salary) OVER(),MIN(salary) OVER() FROM employees;
+SELECT emp_no,department,salary,SUM(salary) OVER(PARTITION BY department),MAX(salary) OVER() FROM employees;
+SELECT emp_no,department,salary,SUM(salary) OVER(PARTITION BY department ORDER BY SALARY DESC),MAX(salary) OVER() FROM employees;
+SELECT emp_no,department,salary,salary-LAG(salary) 
+OVER(PARTITION BY department ORDER BY salary DESC)as salary_diff
+FROM employees
